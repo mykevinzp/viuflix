ce9fdd121c16fd2f0387d235021c84cfcc561829
ogochukwu kevin AKAOSE <49272704+mykevinzp@users.noreply.github.com>
2019-07-24 15:06:46 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+// select tab content item
+function selectItem(e) {
+removeBorder();
+removeShow();
+// add border to current tab
+this.classList.add('tab-border');
+// grab content item from dom
+const tabContentItem = document.getElementById(`${this.id}-content`);
+// add show class
+tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+tabItems.forEach(item => item.classList.remove('tab-border')
+);}
+
+function removeShow() {
+tabContentItems.forEach(item => item.classList.remove('show')
+);}
+
+tabItems.forEach(item => item.addEventListener('click', selectItem));
